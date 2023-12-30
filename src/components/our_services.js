79b021dc5c137ad8d17194704238f298ee5db188
ProInnovationsFoods <150746130+ProInnovationsFoods@users.noreply.github.com>
2023-12-30 12:01:17 -0500

    Images

--- src/components/our_services.js
+++ src/components/our_services.js
@@ -22,7 +22,7 @@ export const OurServices = () => {
                     <li class="cards__item">
                         <div class="card">
                         {/* <div class="card__image card__image--service2"></div> */}
-                            <CarouselImages images={['/service1.jpeg','/service2.png','/service3.png']}></CarouselImages>
+                            <CarouselImages images={['/proccess_automation_service-transformed.jpeg']}></CarouselImages>
                         <div class="card__content d-flex">
                             <div class="card__title mb-3 justify-content-center">Process Automation service</div>
                             <p class="card__text">Embrace the future of manufacturing with our cutting-edge Process Automation Service. We specialize in designing and implementing automation solutions that streamline your operations, increase efficiency, and reduce costs. Our team of experts harnesses the power of technology to create bespoke automation systems, ensuring your processes run seamlessly and your business thrives in the modern era.</p>
@@ -32,7 +32,7 @@ export const OurServices = () => {
                     <li class="cards__item">
                         <div class="card">
                         {/* <div class="card__image card__image--service3"></div> */}
-                            <CarouselImages images={['/service1.jpeg','/service2.png','/service3.png']}></CarouselImages>
+                            <CarouselImages images={['/manufacture_1_transformed.png', '/manufacture_2_transformed.png', '/manufacture_3_transformed.png', 'manufacture_4.jpeg']}></CarouselImages>
                         <div class="card__content">
                             <div class="card__title mb-3">Manufacture of Metal-Mechanical solutions</div>
                             <p class="card__text">Precision and durability are the cornerstones of our Manufacture of Metal-Mechanical Solutions. Whether it's custom machinery components, structural elements, or intricate parts, we leverage advanced manufacturing techniques to produce high-quality metal-mechanical solutions. From concept to fabrication, we take pride in delivering products that meet the highest industry standards.</p>
@@ -41,7 +41,7 @@ export const OurServices = () => {
                     </li>
                     <li class="cards__item">
                         <div class="card">
-                            <CarouselImages images={['/service1.jpeg','/service2.png','/service3.png']}></CarouselImages>
+                            <CarouselImages images={['/engineering_parts-transformed.jpeg','/engineering_parts_2_transformed.png']}></CarouselImages>
                         <div class="card__content">
                             <div class="card__title mb-3 justify-content-center">Engineering Parts service</div>
                             <p class="card__text ">Detail-oriented and client-focused, our Engineering Parts Service is dedicated to providing you with the exact components your project demands. Our skilled engineers work closely with you to understand your specifications, ensuring that the parts we produce not only meet but exceed your expectations. From prototypes to mass production, we deliver precision-engineered parts for a wide range of applications.</p>
